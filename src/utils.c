e0573f2d1d99333b0013d5eedd44c927fefd816a
FM <takeadump@free.fr>
2017-07-24 19:04:29 +0200

    fixed a bug to secure the SDL free

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 10:33:38 by fmessina          #+#    #+#             */
-/*   Updated: 2017/07/22 17:45:17 by fmessina         ###   ########.fr       */
+/*   Updated: 2017/07/24 19:04:04 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ void		error(t_env *e, char *str)
 	ft_putendl(str);
 	if (e)
 	{
-		SDL_DestroyRenderer(e->ren);
-		SDL_DestroyWindow(e->win);
-		SDL_Quit();
+		if (e->win || e->ren != NULL)
+		{
+			(e->ren != NULL ? SDL_DestroyRenderer(e->ren) : 0);
+			(e->win != NULL ? SDL_DestroyWindow(e->win) : 0 );
+			SDL_Quit();
+		}
 		free(e);
 	}
 	exit(EXIT_FAILURE);
