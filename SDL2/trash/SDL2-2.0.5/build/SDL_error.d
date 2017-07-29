61c365b666f2fb58d528b563891d0c673507cb5a
Tac <takeadump@free.fr>
2017-07-29 13:22:06 +0200

    added SDL2 compiled folder in case of offline work and prevent from makefclean bullshit

---
+++ SDL2/trash/SDL2-2.0.5/build/SDL_error.d
@@ -0,0 +1,9 @@
+build/SDL_error.lo: \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/SDL_error.c \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/./SDL_internal.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/dynapi/SDL_dynapi.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h include/SDL_log.h include/SDL_stdinc.h \
+  include/SDL_error.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/SDL_error_c.h
---
+++ SDL2/trash/SDL2-2.0.5/build/SDL_rwopsbundlesupport.d
@@ -0,0 +1,8 @@
+build/SDL_rwopsbundlesupport.lo: \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/file/cocoa/SDL_rwopsbundlesupport.m \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/file/cocoa/../../SDL_internal.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/file/cocoa/../../dynapi/SDL_dynapi.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/file/cocoa/../../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h \
+  /Users/tac/Documents/A_Projects/9_wolf3d/SDL2-2.0.5/src/file/cocoa/SDL_rwopsbundlesupport.h
