dad6b8b1f7ba6d538519d9db12a4fd84585cdd13
FM <takeadump@free.fr>
2017-08-16 17:19:12 +0200

    added all event functions in events.c

---
+++ src/events.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/16 17:01:07 by fmessina          #+#    #+#             */
+/*   Updated: 2017/08/16 17:18:33 by fmessina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+void win_events(t_env *e)
+{
+	if (e->eve.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+	{
+		e->w_w = e->eve.window.data1;
+		e->w_h = e->eve.window.data2;
+		ft_putnbr(e->w_w);
+		ft_putchar('\n');
+		ft_putnbr(e->w_h);
+		ft_putchar('\n');
+	}
+}
+
+void	keypress_events(t_env *e)
+{
+	if (e->eve.key.type == SDL_KEYDOWN)
+	{
+		(e->eve.key.keysym.sym == 27 ? quit(e) : 0);
+	//	(e->eve.key.keysym.sym == 104 ? PrintWindowEvent(&e->eve) : 0);
+	}
+}
