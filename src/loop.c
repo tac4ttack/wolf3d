fed26257baa8642bb49b2402773f457d767229f5
FM <takeadump@free.fr>
2017-08-16 16:53:32 +0200

    added an option for debug

--- src/loop.c
+++ src/loop.c
@@ -6,13 +6,13 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 16:59:00 by fmessina          #+#    #+#             */
-/*   Updated: 2017/08/01 19:46:29 by fmessina         ###   ########.fr       */
+/*   Updated: 2017/08/16 16:47:03 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-void win_events(t_env *e)
+static void win_events(t_env *e)
 {
 	if (e->eve.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
 	{
@@ -21,10 +21,13 @@ void win_events(t_env *e)
 	}
 }
 
-void	keypress_events(t_env *e)
+static void	keypress_events(t_env *e)
 {
 	if (e->eve.key.type == SDL_KEYDOWN)
+	{
 		(e->eve.key.keysym.sym == 27 ? quit(e) : 0);
+	//	(e->eve.key.keysym.sym == 104 ? PrintWindowEvent(&e->eve) : 0);
+	}
 }
 
 
@@ -37,7 +40,7 @@ Uint8	main_loop(t_env *e)
 			{
 				(e->eve.type == SDL_WINDOWEVENT ? win_events(e) : 0);
 				(e->eve.type == SDL_KEYDOWN ? keypress_events(e) : 0);
-//				(e->debug ? DebugFunkyFuncts() : 0);
+				(e->debug ? PrintWindowEvent(&e->eve) : 0);
 				(e->eve.type == SDL_QUIT ? e->run = 0 : 0);
 				(e->run == 0 ? quit(e) : 0);
 			}
