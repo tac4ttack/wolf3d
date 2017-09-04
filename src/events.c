fbf63114aeb0e68b4b83c4cdea2e0fcaabe9afdd
FM <takeadump@free.fr>
2017-09-04 02:16:30 +0200

    added movements to the player, strafing left to do

--- src/events.c
+++ src/events.c
@@ -26,31 +26,10 @@ void	keypress_events(t_env *e)
 	if (e->eve.key.type == SDL_KEYDOWN)
 	{
 		(e->eve.key.keysym.sym == 27 ? quit(e) : 0);
-		
-		(e->eve.key.keysym.sym == 0x40000050 ? e->player.dir -= 1 : 0); // gauche
-		(e->player.dir == -1 ? e->player.dir = 359 : 0);
-		(e->eve.key.keysym.sym == 0x4000004F ? e->player.dir += 1 : 0); // droite
-		(e->player.dir == 361 ? e->player.dir = 1 : 0);
-		if (e->eve.key.keysym.sym == 0x40000052) // haut
-		{
-			e->player.n_pix.x = e->player.pix.x + sinl((e->player.dir * DEG2RAD)) * 10;
-			e->player.n_pix.y = e->player.pix.y - cosl(e->player.dir * DEG2RAD) * 10;
-			if (read_pixels(e, e->player.n_pix.x, e->player.n_pix.y) != 1)
-			{
-				e->player.pix.x = e->player.n_pix.x;
-				e->player.pix.y = e->player.n_pix.y;
-			}
-		}
-		if (e->eve.key.keysym.sym == 0x40000051) // bas
-		{
-			e->player.n_pix.x = e->player.pix.x - sinl((e->player.dir * DEG2RAD)) * 10;
-			e->player.n_pix.y = e->player.pix.y + cosl(e->player.dir * DEG2RAD) * 10;
-			if (read_pixels(e, e->player.n_pix.x, e->player.n_pix.y) != 1)
-			{
-				e->player.pix.x = e->player.n_pix.x;
-				e->player.pix.y = e->player.n_pix.y;
-			}
-		}
+		(e->eve.key.keysym.sym == 0x40000050 ? look(e, -1) : 0); // gauche
+		(e->eve.key.keysym.sym == 0x4000004F ? look(e, 1) : 0); // droite
+		(e->eve.key.keysym.sym == 0x40000052 ? move(e, 1) : 0);
+		(e->eve.key.keysym.sym == 0x40000051 ? move(e, -1) : 0);
 		(e->eve.key.keysym.sym == 104 ? PrintWindowEvent(&e->eve) : 0);
 	}
 }
